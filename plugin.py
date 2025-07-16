c908ac6eee6a4f10a3dab5ee426fbe05655436a1
Annhilati <78727320+annhilati@users.noreply.github.com>
2025-07-16 20:09:25 +0200

    Sync

--- plugin.py
+++ plugin.py
@@ -3,5 +3,12 @@ from beet import Context
 from beetsmith.toolchain.file import load_from_file, CustomItem
 
 def main(ctx: Context):
-    load_from_file(Path("src/data/minecraft/beetsmith/iron_pickaxe.yml")).implement(ctx.data)
-    # CustomItem().encha
+    items = [
+        (Path("src/data/minecraft/beetsmith/iron_pickaxe.yml"), "minecraft:iron_pickaxe")
+    ]
+    
+    
+    for item in items:
+        instance = load_from_file(item[0])
+        instance.item = item[1]
+        instance.implement(ctx.data)
