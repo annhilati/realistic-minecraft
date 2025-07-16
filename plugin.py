7a85ddbf9a1a1d268b65db481915b6834bd7e973
Annhilati <78727320+annhilati@users.noreply.github.com>
2025-07-16 19:21:06 +0200

    Added all can_mine and cannot_mine shorthands

--- plugin.py
+++ plugin.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from beet import Context
+from beetsmith.toolchain.file import load_from_file, CustomItem
 
 def main(ctx: Context):
-    ...
+    load_from_file(Path("src/data/minecraft/beetsmith/iron_pickaxe.yml")).implement(ctx.data)
+    # CustomItem().encha
