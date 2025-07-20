7a25b47e624e64c80431637ffc4da3a00ddd2870
Annhilati <antbe.artis@web.de>
2025-07-20 16:33:39 +0200

    Sync

--- plugin.py
+++ plugin.py
@@ -1,22 +1,25 @@
 import yaml
 from pathlib import Path
 from beet import Context, TagFile, Plugin, NamespaceFileScope
-from beetsmith.toolchain.file import load_from_file, CustomItem
+# from beetsmith.toolchain.file import load_from_file
 from typing import ClassVar
-def main(ctx: Context):
-    items = [
-        (Path("src/items/iron_pickaxe.yml"), "minecraft:iron_pickaxe")
-    ]
+
+# def main(ctx: Context):
+#     items = [
+#         (Path("src/items/iron_pickaxe.yml"), "minecraft:iron_pickaxe")
+#     ]
     
-    for item in items:
-        instance = load_from_file(item[0])
-        instance.item = item[1]
-        instance.implement(ctx.data)
+#     for item in items:
+#         instance = load_from_file(item[0])
+#         instance.item = item[1]
+#         instance.implement(ctx.data)
 class Tag(TagFile):
     scope: ClassVar[NamespaceFileScope] = ("tags",)
+
 def tag_building_requirements(ctx: Context) -> Plugin:
     print("extended")
     ctx.data.extend_namespace.append(Tag)
+
 def build_tags(ctx: Context):
     dp = ctx.data
     
