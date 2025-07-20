0581d79ceae73864089bc6fb3b8cb5895491eb18
Annhilati <antbe.artis@web.de>
2025-07-20 17:21:20 +0200

    tags -> tags/block

--- plugin.py
+++ plugin.py
@@ -1,12 +1,12 @@
 import yaml
 from pathlib import Path
-from beet import Context, TagFile, Plugin, NamespaceFileScope
+from beet import Context, BlockTag, Plugin, NamespaceFileScope
 from beetsmith.toolchain.file import load_from_file
 from typing import ClassVar
 
 def main(ctx: Context):
     items = [
-        (Path("src/items/iron_pickaxe.yml"), "minecraft:iron_pickaxe")
+        (Path("Caveman/items/iron_pickaxe.yml"), "minecraft:iron_pickaxe")
     ]
   
     for item in items:
@@ -14,22 +14,15 @@ def main(ctx: Context):
         instance.item = item[1]
         instance.implement(ctx.data)
 
-class Tag(TagFile):
-    scope: ClassVar[NamespaceFileScope] = ("tags",)
-
-def tag_building_requirements(ctx: Context) -> Plugin:
-    print("extended")
-    ctx.data.extend_namespace.append(Tag)
-
 def build_tags(ctx: Context):
     dp = ctx.data
     
-    with open("src/atlas.yml", "r") as f:
+    with open("Caveman/atlas.yml", "r") as f:
         data: dict[str, dict[float, list[dict]]] = yaml.safe_load(f)
     for tier, hardnesses in data.items():
         for hardness, blocks in hardnesses.items():
             for block_entry in blocks:
                 block = next(iter(block_entry))
                 vanilla_hardness = next(iter(block_entry.values()))
-                dp[Tag].setdefault(f"materials:tier/{tier}/unlocks").merge(Tag({"values": [block]}))
+                dp[BlockTag].setdefault(f"materials:tier/{tier}/unlocks").merge(BlockTag({"values": [block]}))
                 # Missing code for hardness tags
